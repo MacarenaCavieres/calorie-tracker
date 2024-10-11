3ebada5076227977e55a20811c549e8db2aeed99
MCavieres <maca.c.91@gmail.com>
2024-10-11 16:35:13 -0300

    primer commit

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,9 @@
+export type ActivityActions = {};
+
+export type ActivityStateProps = {};
+
+export const initialState = {};
+
+export const activityReducer = (state: ActivityStateProps = initialState, action: ActivityActions) => {
+    return state;
+};

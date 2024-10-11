3483a4490e6c4faca3e17ad852450f0f24fb9ea9
MCavieres <maca.c.91@gmail.com>
2024-10-11 17:12:06 -0300

    se estructura header y form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import Form from "./component/Form";
+
 function App() {
     return (
         <>
-            <h1>Calorie tracker</h1>
+            <header className="bg-cyan-800 py-10 flex justify-around items-center px-3">
+                <h1 className="text-4xl font-bold text-white">Registro de calorías</h1>
+                <button
+                    className="uppercase bg-amber-500 cursor-pointer hover:bg-amber-400
+                 text-white font-bold p-3 rounded-lg"
+                >
+                    Resetear app
+                </button>
+            </header>
+
+            <main className="bg-cyan-700">
+                <section className=" py-20 px-5 max-w-4xl mx-auto">
+                    <Form />
+                </section>
+            </main>
         </>
     );
 }
